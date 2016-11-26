4811f03545713f86a0b6659c55e6e0d62b70d120
Sergei Fonov <ask@mounlion.com>
2016-11-26 21:37:22 +0300

    Вынес игровой процес в отдельную функцию

---
+++ playscope.js
@@ -0,0 +1,27 @@
+/**
+ * Created by csergey on 26.11.16.
+ */
+
+var init = ($) => {
+    $.getUserSession('plays').then(data => {
+        if(isNaN(data)){
+            $.setUserSession('plays', '0')
+        }
+    })
+};
+
+var set = ($, e, callback) => {
+    $.getUserSession('plays').then(data => {
+        if(!isNaN(data)){
+            var scope = e ? parseInt(data)+5 : parseInt(data)-5;
+            $.setUserSession('plays', scope.toString(10));
+            callback(scope)
+        }
+    })
+};
+
+module.exports = {
+    init,
+    set
+}
+
