0dff50d01625844ef93dd86e84d52adcde695734
Sergei Fonov <ask@mounlion.com>
2016-11-26 22:10:18 +0300

    New method stat and clear

--- playscope.js
+++ playscope.js
@@ -20,8 +20,29 @@ var set = ($, e, callback) => {
 	})
 };
 
+var clear = ($) => {
+	if($.message.text && $.message.text == '/clear')
+		$.sendMessage('Ваш игровой прогресс сброшен').then(() => {
+			$.setUserSession('plays', '0')
+		})
+};
+
+var stat = ($) => {
+	if($.message.text && $.message.text == '/stat'){
+		$.getUserSession('plays').then(data => {
+			if(isNaN(data))
+				data = 0
+			$.sendMessage('Ваш рейтинг: <b>'+data+'</b>\n<b>'+$.message.from.firstName+', вы - '+require('./status')(data).toLowerCase()+'</b>', {
+				parse_mode: 'HTML'
+			})
+		})
+	}
+};
+
 module.exports = {
 	init,
-	set
-}
+	set,
+    clear,
+    stat
+};
 
