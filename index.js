1359326b11b16bddc17b8f68851082aa225341b4
Sergei Fonov <ask@mounlion.com>
2016-11-26 16:32:09 +0300

    add

--- index.js
+++ index.js
@@ -13,8 +13,57 @@ const tg = new Telegram.Telegram('328061181:AAGql7NWJcrQ0Y81NaYLBuLqteVffD_RBVc'
 })
 
 class OtherwiseController extends TelegramBaseController {
-    handle() {
-        console.log('otherwise')
+    handle($) {
+
+        var checkmark = (text, messageId) => {
+            var message = ''
+            if(text == 'DeLorean'){
+                message = 'Вы ответили правельно'
+            }else {
+                message = 'Вы ответили не правельно'
+            }
+            tg.api.editMessageText(message, {
+                chat_id: $.chatId,
+                message_id: messageId
+            }).then(() => {
+                this.handle($)
+            })
+        }
+
+
+        $.sendPhoto({ path: './delorian_dmc12.jpg'}).then(() => {
+            $.runInlineMenu({
+                layout: 2,
+                method: 'sendMessage',
+                params: ['Выбирети марку машины'],
+                menu: [
+                    {
+                        text: 'Mazda',
+                        callback: (callbackQuery, message) => {
+                            checkmark(message.text, message.messageId)
+                        }
+                    },
+                    {
+                        text: 'Honda',
+                        callback: (callbackQuery, message) => {
+                            checkmark(message.text, message.messageId)
+                        }
+                    },
+                    {
+                        text: 'DeLorean',
+                        callback: (callbackQuery, message) => {
+                            checkmark(message.text, message.messageId)
+                        }
+                    },
+                    {
+                        text: 'Toyota',
+                        callback: (callbackQuery, message) => {
+                            checkmark(message.text, message.messageId)
+                        }
+                    },
+                ]
+            })
+        })
     }
 }
 
