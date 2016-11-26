2c712bb8b65ee3b53aecb051b2214604b1bfaa8b
Sergei Fonov <ask@mounlion.com>
2016-11-26 16:53:30 +0300

    add player scope

--- index.js
+++ index.js
@@ -15,18 +15,44 @@ const tg = new Telegram.Telegram('328061181:AAGql7NWJcrQ0Y81NaYLBuLqteVffD_RBVc'
 class OtherwiseController extends TelegramBaseController {
     handle($) {
 
+        var initplayscope = () => {
+            $.getUserSession('plays').then(data => {
+                if(isNaN(data)){
+                    $.setUserSession('plays', '0')
+                }
+            })
+        }
+
+        initplayscope()
+
+        var playscope = (e, callback) => {
+
+            $.getUserSession('plays').then(data => {
+                if(!isNaN(data)){
+                    var scope = e ? parseInt(data)+5 : parseInt(data)-5
+                    $.setUserSession('plays', '%d'.format(scope))
+                    callback(scope)
+                }
+            })
+
+        }
+
         var checkmark = (text, messageId) => {
-            var message = ''
-            if(text == 'DeLorean'){
-                message = 'Вы ответили правельно'
-            }else {
-                message = 'Вы ответили не правельно'
-            }
-            tg.api.editMessageText(message, {
-                chat_id: $.chatId,
-                message_id: messageId
-            }).then(() => {
-                this.handle($)
+            var message = '';
+            var f = (text == 'DeLorean') ? true : false;
+            playscope(f, (scope) => {
+                if(f){
+                    message = 'Вы ответили правельно, ваш рейтинг: <b>'+scope+'</b>'
+                }else {
+                    message = 'Вы ответили не правельно, ваш рейтинг: <b>'+scope+'</b>'
+                }
+                tg.api.editMessageText(message, {
+                    chat_id: $.chatId,
+                    message_id: messageId,
+                    parse_mode: 'HTML'
+                }).then(() => {
+                    this.handle($)
+                })
             })
         }
 
@@ -40,25 +66,25 @@ class OtherwiseController extends TelegramBaseController {
                     {
                         text: 'Mazda',
                         callback: (callbackQuery, message) => {
-                            checkmark(message.text, message.messageId)
+                            checkmark('Mazda', message.messageId)
                         }
                     },
                     {
                         text: 'Honda',
                         callback: (callbackQuery, message) => {
-                            checkmark(message.text, message.messageId)
+                            checkmark('Honda', message.messageId)
                         }
                     },
                     {
                         text: 'DeLorean',
                         callback: (callbackQuery, message) => {
-                            checkmark(message.text, message.messageId)
+                            checkmark('DeLorean', message.messageId)
                         }
                     },
                     {
                         text: 'Toyota',
                         callback: (callbackQuery, message) => {
-                            checkmark(message.text, message.messageId)
+                            checkmark('Toyota', message.messageId)
                         }
                     },
                 ]
