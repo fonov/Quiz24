619066a97455ba137dc108bfb04011ec0d178d20
Sergei Fonov <ask@mounlion.com>
2016-11-27 09:59:00 +0300

    Теперь все работает. Добавил сообщение которое сообщает что все картинки в категории просмотренны

---
+++ quiz.js
@@ -0,0 +1,67 @@
+/**
+ * Created by csergey on 27.11.16.
+ */
+
+const store = require('./store')
+const utils = require('./utils')
+
+var quiz = ($) => {
+    store.getData($, (data) => {
+        /*
+        data.latest_urls = ['categories/Cars/Porsche/porsche_1.jpg', 'categories/Cars/Chevrolet/chevrolet_1.jpg']
+        ЭМУЛЯЦИЯ
+        */
+
+        console.log(utils.getCountFilesInCategory(data.category))
+        console.log(data.latest_urls.length)
+
+        //Отсылаем сообщение о том что все картинки просмотренны и не хотет ли чел попробывать себя в другой категории
+        if (utils.getCountFilesInCategory(data.category) == data.latest_urls.length)
+            $.sendMessage('<b>%s</b>, вы прошли всю категорию "%s". Попробуйте другие категории /category'.format($.message.from.firstName, data.question), {
+                parse_mode: 'HTML'
+            }).then(()=>{
+                // мы просмотрели все картинки в категории, очистим массив
+                data.latest_urls = []
+            })
+
+
+        let items = utils.getRandomItems(data.category, data.latest_urls)
+        data.latest_urls.push(items[0])
+
+
+        store.setLatestUrls($, data.latest_urls, () => {
+            let check_answer = (text, messageId) => {
+                let answer = text == items[1]
+                let score = data.score
+                score += answer ? 5 : -5
+                store.setScore($, score, () => {
+                    let msg = ''
+                    if (answer)
+                        msg = 'Вы угадали! Ваш рейтинг: <b>' + score + '</b>'
+                    else
+                        msg = 'Вы не угадали. Ваш рейтинг: <b>' + score + '</b>'
+
+                    msg += '\n<b>' + utils.getRankMsg(score) + '</b>'
+                    tg.api.editMessageText(msg, {chat_id: $.chatId, message_id: messageId, parse_mode: 'HTML'}).then(() => {
+                        require('./quiz').quiz($)
+                    })
+                })
+            }
+
+            $.sendPhoto({path: items[0]}).then(() => {
+                $.runInlineMenu({
+                    layout: 2,
+                    method: 'sendMessage',
+                    params: [data.question],
+                    menu: utils.genMenu(items, check_answer)
+                })
+            })
+        })
+
+    })
+}
+
+module.exports = {
+    quiz
+}
+
